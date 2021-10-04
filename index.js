b151d4febf112f243a9f7aaf30c135b0cb439ef0
OLGA <olichka23.85@gmail.com>
2021-10-04 12:17:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+class Breakfast
+{
+    constructor(egg, juice) {
+        this.food = egg;
+        this.drink = juice;
+    }
+}
+
+class Lunch
+{
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner
+{
+    constructor(salad, soup, entree, _dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = _dessert;
+    }
+}
